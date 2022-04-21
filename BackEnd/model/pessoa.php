497a95c0909c3ca95a0aa9597e17f26e3ea54d2b
julianosilveira-2 <60629167+julianosilveira-2@users.noreply.github.com>
2022-04-20 22:20:50 -0300

    Inserindo "tipagem" no construct

--- BackEnd/model/pessoa.php
+++ BackEnd/model/pessoa.php
@@ -2,7 +2,7 @@
 	class Pessoa {
 		public $cpf, $nome, $telefone, $email, $senha;
 
-		public function __construct($cpf, $nome, $telefone, $email, $senha) {
+		public function __construct(string $cpf, string $nome, string $telefone, string $email, string $senha) {
 			$this->cpf = $cpf;
 			$this->nome = $nome;
 			$this->telefone = $telefone;
