7ff7fc4c3e977bdd191501b2159c13866d87c125
Camila Volponi <camilavolponi19@gmail.com>
2022-04-20 22:24:10 -0300

    Inserindo "tipagem" no construct

--- BackEnd/model/endereco.php
+++ BackEnd/model/endereco.php
@@ -1,15 +1,15 @@
 <?php
-	class Endereco {
-		private $cep, $rua, $numero, $bairro, $cidade, $complemento, $referencia;
+class Endereco {
+	private $cep, $rua, $numero, $bairro, $cidade, $complemento, $referencia;
 
-		public function __construct($cep, $rua, $numero, $bairro, $cidade, $complemento, $referencia) {
-			$this->cep = $cep;
-			$this->rua = $rua;
-			$this->numero = $numero;
-			$this->bairro = $bairro;
-			$this->cidade = $cidade;
-			$this->complemento = $complemento;
-			$this->referencia = $referencia;
-		}
+	public function __construct(string $cep, string $rua, int $numero, string $bairro, string $cidade, string $complemento, string $referencia) {
+		$this->cep = $cep;
+		$this->rua = $rua;
+		$this->numero = $numero;
+		$this->bairro = $bairro;
+		$this->cidade = $cidade;
+		$this->complemento = $complemento;
+		$this->referencia = $referencia;
 	}
+}
 ?>
