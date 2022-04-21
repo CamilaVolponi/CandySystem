5174a5af228ebc2637b278651fb2b7e44bd7561b
Camila Volponi <camilavolponi19@gmail.com>
2022-04-20 22:16:34 -0300

    Inserindo "tipagem" no construct

--- BackEnd/model/funcionario.php
+++ BackEnd/model/funcionario.php
@@ -1,8 +1,11 @@
 <?php
+
+require_once "pessoa.php";
+
 class Funcionario extends Pessoa {
     public $cargo;
 
-    public function __construct($cargo) {
+    public function __construct(string $cargo, string $cpf, string $nome, string $telefone, string $email, string $senha ) {
         parent::__construct($cpf, $nome, $telefone, $email, $senha);
         $this->cargo = $cargo;
     }
