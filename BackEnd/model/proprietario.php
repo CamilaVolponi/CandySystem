25e3fca7ebfc612779b4819c9120c3bfd6c752c9
Camila Volponi <camilavolponi19@gmail.com>
2022-04-20 22:25:24 -0300

    Inserindo "require_once"

--- BackEnd/model/proprietario.php
+++ BackEnd/model/proprietario.php
@@ -1,7 +1,9 @@
 <?php
+require_once "pessoa.php";
+
 class Proprietario extends Pessoa {
-	public function __construct() {
-		parent::__construct(string $cpf, string $nome, strinhg $telefone, string $email, string $senha);
+	public function __construct(string $cpf, string $nome, strinhg $telefone, string $email, string $senha) {
+		parent::__construct($cpf, $nome, $telefone, $email, $senha);
 	}
 }
 ?>
