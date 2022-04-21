d398bdf81dadba535d6cc9769820ed88ef924fe5
Camila Volponi <camilavolponi19@gmail.com>
2022-04-20 22:20:55 -0300

    Inserindo "tipagem" no construct

--- BackEnd/model/cliente.php
+++ BackEnd/model/cliente.php
@@ -1,9 +1,11 @@
 <?php
-	class Cliente {
-		private $nome, $telefone;
 
-		public function __construct($nome, $telefone) {
-			$this->nome = $nome;
-			$this->telefone = $telefone;
-		}
+class Cliente {
+	private $nome, $telefone;
+
+	public function __construct(string $nome, string $telefone) {
+		$this->nome = $nome;
+		$this->telefone = $telefone;
+}
+	
 ?>
