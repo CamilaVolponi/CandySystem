78b4e1cb1af399596ce85409395c7a0c1d72b8e2
julianosilveira-2 <60629167+julianosilveira-2@users.noreply.github.com>
2022-04-20 22:19:15 -0300

    Inserindo "tipagem" no construct

--- BackEnd/model/produto.php
+++ BackEnd/model/produto.php
@@ -2,7 +2,7 @@
 	class Produto {
 		private $nome, $preco;
 
-		public function __construct($nome, $preco) {
+		public function __construct(string $nome, float $preco) {
 			$this->nome = $nome;
 			$this->preco = $preco;
 		}
